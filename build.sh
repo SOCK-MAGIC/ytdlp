2c0650499bfc5dd0557e2e44a330fc3091345c48
sock_magic <startxingcheng@gmail.com>
2024-09-08 21:33:54 +0800

    test ci

--- build.sh
+++ build.sh
@@ -2,21 +2,27 @@
 
 git clone --depth 1 https://github.com/termux/termux-packages.git
 
-cp build-python /termux-packages
+echo "copy sh file"
+cp build-python.sh /termux-packages
 cp move_accordingto_arch.sh /termux-packages/output
 cp deb-unpackage.sh /termux-packages/output
 
+echo "create dir"
 chmod 777 termux-packages
 cd termux-packages
 mkdir output
 chmod 777 output
 
+echo "run docker"
 ./scripts/run-docker.sh ./clean.sh
+echo "run docker"
 ./scripts/run-docker.sh ./build-python.sh
 
+echo "move"
 cd output
 ./move_accordingto_arch.sh
 
+echo "unpackage"
 ./deb-unpackage.sh -d ./aarch64
 ./deb-unpackage.sh -d ./arm
 ./deb-unpackage.sh -d ./i686
