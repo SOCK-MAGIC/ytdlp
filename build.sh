54081e3cc41f5bc7dfc6c7cde818a29c40462ccc
sock_magic <startxingcheng@gmail.com>
2024-09-08 23:09:12 +0800

    test ci

--- build.sh
+++ build.sh
@@ -31,5 +31,14 @@ echo "unpackage"
 ./deb-unpackage.sh -d ./i686
 ./deb-unpackage.sh -d ./x86_64
 
-ls
-ll
+cp aarch64/data/data/com.termux/files/usr/bin/python3.11 /tmp/libpython_aarch64.so
+zip --symlinks -r /tmp/python3_11_aarch64.zip usr/lib usr/etc
+
+cp arm/data/data/com.termux/files/usr/bin/python3.11 /tmp/libpython_arm.so
+zip --symlinks -r /tmp/python3_11_arm.zip usr/lib usr/etc
+
+cp i686/data/data/com.termux/files/usr/bin/python3.11 /tmp/libpython_i686.so
+zip --symlinks -r /tmp/python3_11_i686.zip usr/lib usr/etc
+
+cp x86_64/data/data/com.termux/files/usr/bin/python3.11 /tmp/libpython_x86_64.so
+zip --symlinks -r /tmp/python3_11_x86_64.zip usr/lib usr/etc
