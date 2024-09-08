64aac6efceae9c2909f049ea8ac7c4c946781de5
sock_magic <startxingcheng@gmail.com>
2024-09-08 23:39:18 +0800

    test ci

--- build.sh
+++ build.sh
@@ -25,20 +25,27 @@ echo "move"
 cd output
 ./move_accordingto_arch.sh
 
-echo "unpackage"
+echo "unpackage aarch64"
 ./deb-unpackage.sh -d ./aarch64
+echo "unpackage arm"
 ./deb-unpackage.sh -d ./arm
+echo "unpackage i686"
 ./deb-unpackage.sh -d ./i686
+echo "unpackage x86_64"
 ./deb-unpackage.sh -d ./x86_64
 
+echo "unpackage aarch64"
 cp aarch64/data/data/com.termux/files/usr/bin/python3.11 /tmp/libpython_aarch64.so
 zip --symlinks -r /tmp/python3_11_aarch64.zip usr/lib usr/etc
 
+echo "unpackage arm"
 cp arm/data/data/com.termux/files/usr/bin/python3.11 /tmp/libpython_arm.so
 zip --symlinks -r /tmp/python3_11_arm.zip usr/lib usr/etc
 
+echo "unpackage i686"
 cp i686/data/data/com.termux/files/usr/bin/python3.11 /tmp/libpython_i686.so
 zip --symlinks -r /tmp/python3_11_i686.zip usr/lib usr/etc
 
+echo "unpackage x86_64"
 cp x86_64/data/data/com.termux/files/usr/bin/python3.11 /tmp/libpython_x86_64.so
 zip --symlinks -r /tmp/python3_11_x86_64.zip usr/lib usr/etc
